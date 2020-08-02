7f83be8480be02c0f4783fe738837160cc78fcec
danton03 <37675973+danton03@users.noreply.github.com>
2020-08-02 02:22:52 -0300

    Aula 02 - parte 1
    Página construída até os 26 min de video da aula 02

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/index';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
 
 
+const Pagina404 = () => (<div><h1>Erro 404 - Página não Encontrada</h1></div>);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component= {Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component= {Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
