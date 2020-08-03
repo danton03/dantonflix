7f80625ee555492595da7d8e9342a5ee1bbca565
danton03 <37675973+danton03@users.noreply.github.com>
2020-08-03 02:38:52 -0300

    Commit final da aula 2
    Alterações na pagina home e criação da parte inicial das páginas de cadastro de video

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Home from './pages/Home/index';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
 
 
 const Pagina404 = () => (<div><h1>Erro 404 - Página não Encontrada</h1></div>);
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component= {Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component= {Pagina404}/>
     </Switch>
   </BrowserRouter>,
